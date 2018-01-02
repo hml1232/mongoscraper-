7a0a2cfb74e9513a0c42669608b9208782e9fa7b
Haley Lock <haleylock@Haleys-Air.fios-router.home>
2018-01-02 17:11:22 -0500

    heroku

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 // Requiring our Note and Article models
-var note = require("./models/note.js");
-var article = require("./models/article.js");
+var note = require("../models/note.js");
+var article = require("../models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
